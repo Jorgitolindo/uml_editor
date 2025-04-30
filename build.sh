c917f6f7f3865b30ce3b9c5529412b516c98566c
Timoteo Ponce <ponce.timoteo@gmail.com>
2025-04-29 21:59:38 -0400

    Updates build script

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 (mkdir backend/src/main/resources/static || true) && cd frontend &&
-  npm i && npm run build --prod 
+  npm i && npm run build 
+# --prod 
   # &&
   # cp -v dist/frontend/browser/* ../backend/src/main/resources/static/. &&
   # cd ../backend && ./mvnw clean package
